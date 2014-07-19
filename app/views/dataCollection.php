c789f5ff524175df89d1c866ef9048091ea4b583
Vera Lin <oyevera@gmail.com>
2014-07-19 16:41:18 -0400

    improve UI and add in test data

--- app/views/dataCollection.php
+++ app/views/dataCollection.php
@@ -15,6 +15,7 @@
 		}
 	</style>
 
+
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
@@ -27,67 +28,95 @@
 
 	<!-- Google Map API -->
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <style type="text/css">
-      html { height: 100% }
-      body { height: 100%; margin: 0; padding: 0 }
-      #map-canvas { height: 100% }
-    </style>
-    <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC0pad4u9RPL2kFoJI5IwAB5yhohxYzrbU">
-    </script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
-    <script type="text/javascript">
+	<style type="text/css">
+		html { height: 100% }
+		body { height: 100%; margin: 0; padding: 0 }
+		#map-canvas { height: 100% }
+	</style>
+	<script type="text/javascript"
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC0pad4u9RPL2kFoJI5IwAB5yhohxYzrbU">
+</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+<script type="text/javascript">
 
-    var canada = new google.maps.LatLng(59.260, -112.496);
+	var locations = [
+	[55,-100],
+	[40,-120],
+	[80.-80],
+	[97,-100],
+	[55,-160]
+	];
+
+	var canada = new google.maps.LatLng(59.260, -112.496);
 	var markerPos = new google.maps.LatLng(59.260, -112.496);
 	var marker;
 	var map;
 
 	function initialize() {
-	  var mapOptions = {zoom: 4, center: canada};
-	  map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+		var mapOptions = {zoom: 4, center: canada};
+		map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
-	  marker = new google.maps.Marker({
-	    map:map,
-	    draggable:true,
-	    animation: google.maps.Animation.DROP,
-	    position: markerPos
-	  });
-	  google.maps.event.addListener(marker, 'click', showChat);
-	  google.maps.event.addListener(map, 'click', hideChat);
+		marker = new google.maps.Marker({
+			map:map,
+			draggable:true,
+			animation: google.maps.Animation.DROP,
+			position: markerPos
+		});
+		google.maps.event.addListener(marker, 'click', showChat);
+		google.maps.event.addListener(map, 'click', hideChat);
+
+
+
+		var i;
+		for (i = 0; i < locations.length; i++) {  
+			marker = new google.maps.Marker({
+				draggable:true,
+				animation: google.maps.Animation.DROP,
+				position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+				map: map
+			});
+			google.maps.event.addListener(marker, 'click', showChat);
+			google.maps.event.addListener(map, 'click', hideChat);
+		}
 	}
 
-	function showChat() {
 
-	  $('#chat').show();
+	function showChat() {
+		$('#chat').show();
 	}
 
 	function hideChat() {
-	   $('#chat').hide();
-	  
+		$('#chat').hide();
+
 	}
 
 	google.maps.event.addDomListener(window, 'load', initialize);
 
 
-    </script>
-    <!-- Google Map API -->
+</script>
+<!-- Google Map API -->
 
 
 
 
 </head>
 <body>
-	
-	<h1>Data Management</h1>
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+		<h1>Data Management</h1>
+		<p>
+			<span class="glyphicon glyphicon-map-marker"></span>
+			Location Map  
+			<a class = "glyphicon glyphicon-refresh" href="data"></a></p>
+		</div>
+	</nav>
 
-	<p> <span class="glyphicon glyphicon-map-marker"></span>Location Map</p>
+
 	<div id='chat' style = " display:none; z-index:1000; background-color:rgba(255,255,255,0.7); width:60%; height:60%; position: absolute; top: 25%; left:20%; right:20%; overflow-y:scroll;">CHAT TEST</div>
 
 
 	<!-- Google Map API -->
-	<div id="map-canvas">
-	</div>
+	<div id="map-canvas"></div>
 	<!-- Google Map API -->
 
 
